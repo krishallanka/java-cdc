0cb33d7011bb97c10cb6d45f8ef988d2c731fbf5
krishallanka <151901657+krishallanka@users.noreply.github.com>
2024-11-08 10:08:29 +0530

    Create String_Creation.java

---
+++ String_Creation.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+public class String_Demo{
+    public static void main (String args[]){
+        String s1="Welcome";
+        char ch[]={'R','A','M'} ;
+        String s2= new String(ch);
+        String s3= new String("Example program");
+        System.out.println("First String is : "+s1);
+        System.out.println("Second string is : "+s2);
+        System.out.println("Third String is : "+s3); 
+}
+}
